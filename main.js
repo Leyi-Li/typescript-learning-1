b4ab2571424c6442f8213d0e28c75952ecda9337
leyla li <leylali@qq.com>
2020-03-19 09:49:49 -0700

    crate a sentence with multiple lines

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log(message);
 var isBeginner = true;
 var total = 0;
 var name = 'Leyla';
+//new line just hit return
+var sentence = "My name is " + name + "\nI am new to typescript";
+console.log(sentence);
--- main.ts
+++ main.ts
@@ -6,3 +6,8 @@ console.log(message);
 let isBeginner: boolean = true;
 let total: number = 0;
 let name: string = 'Leyla';
+
+//new line just hit return
+let sentence: string = `My name is ${name}
+I am new to typescript`;
+console.log(sentence);
