dba79811c7e0ffcb5b73724f7e48f3a14ca78f85
leyla li <leylali@qq.com>
2020-03-19 09:47:59 -0700

    different type of variables

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
 exports.__esModule = true;
 var message = 'hello Leyla';
 console.log(message);
+var isBeginner = true;
+var total = 0;
+var name = 'Leyla';
--- main.ts
+++ main.ts
@@ -2,3 +2,7 @@ export{}
 
 let message = 'hello Leyla';
 console.log(message);
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Leyla';
